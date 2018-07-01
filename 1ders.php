9e0bb34e3a172cf167336eca922f300ae360ff9f
Kemal Ayhan <kemalayhan013@gmail.com>
2018-07-01 12:47:53 +0300

    1.ders tamamlandı.

--- 1ders.php
+++ 1ders.php
@@ -1,3 +1,28 @@
 <?php
 
-bu dosya değişkenler çalışması içerecektir
+// Değişkenler
+
+$isim = "Kemal";
+$soyisim = "Ayhan";
+$eposta = "kemalayhan013@gmail.com";
+$ortalama = 2.9;
+
+echo $isim." ".$soyisim;
+
+$dizi = array('cokluortam' => 'A2', 'ozelogretim' => 'B3', 'toplumahizmet' => 'C1');
+
+$ad = array('isim' => 'kemal', 'soyad' => 'ayhan');
+
+class sinif {
+ var $isim = 'Kemall';
+ var $soyad = 'Ayhan';
+ var $ortalama = 2.9;
+}
+
+$sinif = new sinif();
+
+$sayi1 = 12;
+$sayi2 = 5;
+$sonuc = $sayi1/$sayi2;
+
+
