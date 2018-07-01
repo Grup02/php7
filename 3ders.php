30912bf23905fef6f17f9bb2c3cf721dc730a4a4
Kemal Ayhan <kemalayhan013@gmail.com>
2018-07-01 13:34:21 +0300

    döngüler bitti

--- 3ders.php
+++ 3ders.php
@@ -1,1 +1,37 @@
-Bu haftaki ödevler
+<?php 
+ for ($i = 0; $i < 10 ; $i++)
+ 	 { 
+ 	 	echo $i."</br>";
+ 	 }
+
+
+// ikinci örnek
+
+ 	 $dizi = array("kemal","ayhan","hatice","ergün","ferah","zwe");
+
+ 	 for ($i = 0; $i < count($dizi) ; $i++)
+ 	  { 
+ 	 	echo $dizi[$i]."</br>";
+ 	 }
+
+ // Üçüncü örnek
+
+ 	 foreach ($dizi as $d) {
+ 	 	echo $d."</br>";
+ 	 }
+
+ // Dördüncü Örnek
+ 
+ $isim = "hatice";
+ switch ($isim) {
+ 	 	case 'kemal':
+ 	 		echo "admin girişi yapıldı.";
+ 	 		break;
+
+ 	 	case 'hatice':
+ 	 		echo "kullanıcı giriş yapıldı";
+ 	 		break;
+ 	 	default:
+ 	 		echo "giriş yapılmadı";
+ 	 		break;
+ 	 }	 
