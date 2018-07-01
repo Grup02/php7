8649c67c2340146bc1d54c4cc37ca1a62980b3da
Kemal Ayhan <kemalayhan013@gmail.com>
2018-07-01 13:15:28 +0300

    koşullu ifadeler

--- 2ders.php
+++ 2ders.php
@@ -1,3 +1,34 @@
 <?php
 
-bu dosya koşulu ifadeler konusu wsdfghjkl
+$sayi1 = 10;
+$sayi2 = 20;
+if($sayi1 > $sayi2)
+{
+	echo $sayi1. " büyüktür" .$sayi2. "'den";
+}
+else
+{
+	echo $sayi1. " küçüktür" .$sayi2. "'den </br>";
+}
+
+
+//İkinci Örnek
+
+$kullaniciadi = "kemavghghlayhan";
+if ($kullaniciadi != "kemalayhan")
+{
+	echo "giriş yapıldı.";
+}
+else 
+{
+	echo "kullanıcı adı yanlış. </br>";
+}
+
+//Üçüncü Örnek
+
+ function toplama($a,$b)
+{
+	echo $a + $b;
+}
+
+toplama(4,3);
