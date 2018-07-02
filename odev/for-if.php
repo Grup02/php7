4619602895e1d707e7ec86f02b6d93397a0b4028
Kemal Ayhan <kemalayhan013@gmail.com>
2018-07-02 14:03:05 +0300

    acıklama satırları eklendi

--- odev/for-if.php
+++ odev/for-if.php
@@ -14,7 +14,7 @@ echo "</br>"."İkinci Örnek"."</br>";
 
 
 
-$dizi = array('kemal','ayhan','mozart','ludvig');
+$dizi = array('kemal','ayhan','mozart','ludwig');
 
 foreach ($dizi as $a)
 	{
@@ -45,7 +45,8 @@ foreach ($dizi as $a)
 echo "</br>"."Dörtüncü Örnek"."</br>";
 	// Dörtüncü Örnek	
 	$ad = "Kemal";
-	$soyad = "";
+	$soyad = ""; 
+	// empty fonksiyonu değişkenin değerinin boş olup olmadığına bakar.
 	if (empty($ad)) { echo "İsminiz mevcut değil"; } else { echo "İsminiz Mevcut "; }
 	echo "</br>";
 	if (empty($soyad)) { echo "Soy isminiz mevcut değil . </br>"; } else { echo "Soy isminiz Mevcut "; }
@@ -55,7 +56,7 @@ echo "</br>"."Dörtüncü Örnek"."</br>";
 	echo "</br>"."Beşinci Örnek"."</br>";
 	// Beşinci Örnek	
 	$temizlik = "unset ne işe yarar";
-	unset($temizlik);
+	unset($temizlik); // parametre olarak verilen değişkeni hafızadan siler.
 	if(isset($temizlik))
 	{
 		echo "unset çalışmıyor.";
@@ -70,16 +71,15 @@ echo "</br>"."Dörtüncü Örnek"."</br>";
 	echo "</br>"."Altıncı Örnek"."</br>";
 	// Altıncı Örnek
 	 $parcalan = "Kemal,Ayhan,11.96,Ankara";
-
-	 $kk = explode(',', $parcalan);
+	 $kk = explode(',', $parcalan); // değişkeni bir ayraca göre ayırıp diziye atar. ilk parametre ayraç, 2. parametre değişken
 	 print_r($kk);
 	 echo "</br>";
 
 echo "</br>"."Yedinci Örnek"."</br>";
 	// Yedinci Örnek
 
-echo "Bugünün tarihi ".date("d/m/y")."</br>";
-echo "Bugünün tarihi ".date("D/M/Y")."</br>";
+echo "Bugünün tarihi ".date("d/m/y")."</br>";  // rakamlarla yazıyor.
+echo "Bugünün tarihi ".date("D/M/Y")."</br>";   // harflerle yazıyor.
 echo "Şuan saat ".date("h:i:sa")."</br>";
 
 
@@ -88,7 +88,15 @@ echo "</br>"."Sekizinci Örnek"."</br>";
 	// Sekizinci Örnek
 
 $bosluksuz = "    kemal";
-echo trim($bosluksuz)."</br>";
+echo trim($bosluksuz)."</br>"; // boşlukları siler.
 
 $fyo = 555;
-echo var_dump($fyo);
+echo var_dump($fyo); // değişkenin türünü ve değerini gösterir.
+
+echo "</br>"."Dokuzuncu Örnek"."</br>";
+	// Dokuzuncu Örnek
+
+$cars = array('Mercedes','Audi','Bmw');// dizi elemanları değişkene atar. İlk parametre ayraç
+
+print_r(implode(' ',$cars));
+
